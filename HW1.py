7f1b8dd3e48175229a69f6c22016b9f11efa4218
itamar3113 <itamar3113@gmail.com>
2024-11-27 12:05:58 +0200

    improved minkowski sum and get visibility graph

--- HW1.py
+++ HW1.py
@@ -6,12 +6,8 @@ from Plotter import Plotter
 from shapely.geometry.polygon import Polygon, LineString
 
 
+# TODO remove in is not needed
 def sort_vertices_counterclockwise(polygon):
-    # center_x, center_y = polygon.mean(0)
-    # x, y = polygon.T
-    # angles = np.arctan2(y, x)
-    # indices = np.argsort(angles)
-    # return polygon[indices]
     return polygon
 
 
@@ -27,27 +23,34 @@ def get_minkowsky_sum(original_shape: Polygon, r: float) -> Polygon:
     :return: The polygon composed from the Minkowsky sums
     """
     vertices = original_shape.exterior.coords[:-1]
-    vertices = sort_vertices_counterclockwise(vertices)
     robot_in_origin = [(0, -r), (r, 0), (0, r), (-r, 0)]
-    i, j = 0, 0
     res = []
-    len_poly = len(vertices)
-    len_robot = len(robot_in_origin)
-    p1, next_poly = vertices[i]
-    p2, next_robot = robot_in_origin[j]
-    while i < len_poly or j < len_robot:
-        p1 = vertices[i % len_poly]
-        p2 = robot_in_origin[j % len_robot]
-        res.append((p1[0] + p2[0], p1[1] + p2[1]))
-        cross = cross_product((vertices[(i + 1) % len_poly][0] - vertices[i % len_poly][0],
-                               vertices[(i + 1) % len_poly][1] - vertices[i % len_poly][1]),
-                              (robot_in_origin[(j + 1) % len_robot][0] - robot_in_origin[j % len_robot][0],
-                               robot_in_origin[(j + 1) % len_robot][1] - robot_in_origin[j % len_robot][1]))
-        if cross >= 0:
-            i += 1
-        if cross <= 0:
-            j += 1
-    return Polygon(res)
+    for p1 in vertices:
+        for p2 in robot_in_origin:
+            res.append((p1[0] + p2[0], p1[1] + p2[1]))
+    return Polygon(res).convex_hull
+    #T ODO check about comlexity and using library functions.
+
+    # other way to compute minkowsky sum
+    # i, j = 0, 0
+    # res = []
+    # len_poly = len(vertices)
+    # len_robot = len(robot_in_origin)
+    # p1, next_poly = vertices[i]
+    # p2, next_robot = robot_in_origin[j]
+    # while i < len_poly or j < len_robot:
+    #     p1 = vertices[i % len_poly]
+    #     p2 = robot_in_origin[j % len_robot]
+    #     res.append((p1[0] + p2[0], p1[1] + p2[1]))
+    #     cross = cross_product((vertices[(i + 1) % len_poly][0] - vertices[i % len_poly][0],
+    #                            vertices[(i + 1) % len_poly][1] - vertices[i % len_poly][1]),
+    #                           (robot_in_origin[(j + 1) % len_robot][0] - robot_in_origin[j % len_robot][0],
+    #                            robot_in_origin[(j + 1) % len_robot][1] - robot_in_origin[j % len_robot][1]))
+    #     if cross >= 0:
+    #         i += 1
+    #     if cross <= 0:
+    #         j += 1
+    # return Polygon(res)
 
 
 # TODO
@@ -59,7 +62,41 @@ def get_visibility_graph(obstacles: List[Polygon], source=None, dest=None) -> Li
     :param dest: The destination of the query. None for part 1.
     :return: A list of LineStrings holding the edges of the visibility graph
     """
-    pass
+    res = []
+    obstacles_edges = []
+    # loop over all the obstacles and add their edges to the map.
+    for obstacle in obstacles:
+        vertices = obstacle.exterior.coords[:]
+        j = 0
+        edge = LineString([vertices[j], vertices[j + 1]])
+        while j < len(vertices) - 1:
+            edge = LineString([vertices[j], vertices[j + 1]])
+            res.append(edge)
+            obstacles_edges.append(edge)
+            j += 1
+    # loop over all vertices in obstacles
+    for i in range(len(obstacles)):
+        vertices = obstacles[i].exterior.coords[:-1]
+        for vertex in vertices:
+            # check for all the vertexes in different polygons if they have visibility line.
+            for k in range(len(obstacles)):
+                if k == i:
+                    continue
+                other_vertices = obstacles[k].exterior.coords[:-1]
+                for other_vertex in other_vertices:
+                    edge = LineString([vertex, other_vertex])
+                    is_visible = True
+                    # check if the edge intersect with one of the obstacles edges.
+                    for obstacle_edge in obstacles_edges:
+                        # if the edges have a common edge skip it.
+                        if vertex in obstacle_edge.coords or other_vertex in obstacle_edge.coords:
+                            continue
+                        if edge.intersects(obstacle_edge):
+                            is_visible = False
+                            break
+                    if is_visible:
+                        res.append(edge)
+    return res
 
 
 def is_valid_file(parser, arg):
@@ -106,20 +143,20 @@ if __name__ == '__main__':
     plotter1.add_c_space_obstacles(c_space_obstacles)
     plotter1.add_robot(source, dist)
 
-    plotter1.show_graph()
+    #plotter1.show_graph()
 
     # step 2:
 
-    # lines = get_visibility_graph(c_space_obstacles)
-    # plotter2 = Plotter()
-    #
-    # plotter2.add_obstacles(workspace_obstacles)
-    # plotter2.add_c_space_obstacles(c_space_obstacles)
-    # plotter2.add_visibility_graph(lines)
-    # plotter2.add_robot(source, dist)
-    #
-    # plotter2.show_graph()
-    #
+    lines = get_visibility_graph(c_space_obstacles)
+    plotter2 = Plotter()
+
+    plotter2.add_obstacles(workspace_obstacles)
+    plotter2.add_c_space_obstacles(c_space_obstacles)
+    plotter2.add_visibility_graph(lines)
+    plotter2.add_robot(source, dist)
+
+    plotter2.show_graph()
+
     # # step 3:
     # with open(query, 'r') as f:
     #     dest = tuple(map(float, f.readline().split(',')))
