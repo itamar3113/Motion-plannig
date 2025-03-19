a42889c2feddecd531a3c4aa9bd5e42bbf091ddc
itamar3113 <itamar3113@gmail.com>
2025-03-19 21:44:32 +0200

    fixed rotation params

--- hw4/params.py
+++ hw4/params.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-meeting_height = 0.5
+meeting_height = 0.6
 meeting_gap = 0.1
-right_angles = (0, -np.pi / 2, -np.pi / 2)
-left_angles = (np.pi, np.pi / 2, np.pi / 2)
+right_angles = (0, -np.pi / 2, 0)
+left_angles = (0, np.pi / 2, 0)
 before_pickup_height = 0.14
 pickup_angles = (0, -np.pi, 0)
 pickup_lowering = 0.12
